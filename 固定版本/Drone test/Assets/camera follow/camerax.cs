e6c3298d9c6c26a159bce086aac83c633db58db3
CKY1015 <30516474+CKY1015@users.noreply.github.com>
2019-08-21 14:05:42 +0800

    add fixed version

---
+++ 固定版本/Drone test/Assets/camera follow/camerax.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camerax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public Transform target;
+    public float relativeHeigth = 0;
+    public float xDistance = 5.0f;
+    public float dampSpeed = 2;
+    // Update is called once per frame
+    void Update () {
+        Vector3 newPos = target.position+ new Vector3(xDistance, relativeHeigth, 0);
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * dampSpeed);
+    }
+}
---
+++ 固定版本/Drone test/Assets/camera follow/cameraz.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraz : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    public Transform target;
+    public float relativeHeigth = 10.0f;
+    public float zDistance = 5.0f;
+    public float dampSpeed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = target.position + new Vector3(0, relativeHeigth, zDistance);
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * dampSpeed);
+    }
+}
